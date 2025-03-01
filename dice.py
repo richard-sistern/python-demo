452a5c9b9c1a06e29ef605235aa63c3fc3eb1172
Richard Sistern <25385580+richard-sistern@users.noreply.github.com>
2025-03-01 15:50:18 +0000

    Done

--- dice.py
+++ dice.py
@@ -1,1 +1,15 @@
 # Roll a dice
+
+import random
+
+def role_dice(sides):
+    valid_dice = {4, 6, 8, 10, 12, 20, 100}
+    
+    if sides not in valid_dice:
+        raise ValueError(f"Invalid dice! Choose from {valid_dice}.")
+
+    return random.randint(1, sides)
+
+#
+print(role_dice(6))
+print(role_dice(20))
--- hello.py
+++ hello.py
@@ -1,2 +1,5 @@
-# Say hello
+# Run with CTRL + F5
 
+# It's not going to do a lot atm so , say 'hello'
+
+print('Hello')
